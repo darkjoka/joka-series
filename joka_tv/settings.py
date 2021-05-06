08477981001f1e0deaed8c9cb63f9571e32a3115
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-05-06 00:05:31 +0000

    change debug setting

--- joka_tv/settings.py
+++ joka_tv/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "c7va!tyqeglf@#!+^(l9yet^kenl*g7-xfi6l+v0l50ei0ey8+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
