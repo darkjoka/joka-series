77f6d7ff394e3f69b01cd62fc11910837668b2ca
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-05-20 21:26:10 +0000

    feat: parse content in getFromIndex

--- server/joka_series/scrappers/scrapers.py
+++ server/joka_series/scrappers/scrapers.py
@@ -1,5 +1,14 @@
 from typing import Dict, List
 from bs4 import BeautifulSoup
+import requests
 
 
 movie = Dict[str, str]
+
+
+def getFromIndex(pageLink: str):
+    mime = requests.get(pageLink)
+    soup = BeautifulSoup(mime.content, "html.parser")
+    articles = soup.find_all(class_="uk-article")
+
+    series: List[movie] = []
