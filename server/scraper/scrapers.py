5ef79335af004ca18cee6f130d61192d33732b16
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-05-24 20:16:26 +0000

    feat: add episode scraping

--- server/scraper/scrapers.py
+++ server/scraper/scrapers.py
@@ -12,3 +12,11 @@ def getFromIndex(pageLink: str):
     articles = soup.find_all(class_="uk-article")
 
     series: List[movie] = []
+
+    for article in articles:
+        title: str = article.find(class_="uk-article-title1").get_text().strip()
+        permaLink: str = article.get("data-permalink")
+        imageSrc: str = article.find("img").get("src")
+        lastEpisode: str = article.find("time").get("datetime")
+        rating: str = article.find(class_="current-rating").get_text().strip()
+        teaser: str = article.find(class_="teasershort").get_text().strip()
