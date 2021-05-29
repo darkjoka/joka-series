6117c44bc7f7a2e616ec89b065ebd6fda853d48c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-05-29 14:27:24 +0000

    feat: remove popular path from urlpatterns

--- server/joka_series/urls.py
+++ server/joka_series/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'joka_series'
+app_name = "joka_series"
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('popular/' ,views.popular, name ='popular'),
-    path('search/', views.search , name = 'search'),
-    path('detail/<str:series>/',views.detail, name='detail'),
+    path("", views.index, name="index"),
+    path("search/", views.search, name="search"),
+    path("detail/<str:series>/", views.detail, name="detail"),
 ]
