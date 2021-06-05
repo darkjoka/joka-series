735a7ba04158aba78d4f5a59918876f9fc2c127d
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-05 17:15:18 +0000

    feat: add content component

---
+++ client/src/components/layout/Content.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const Content = () => {
+  return (
+    <StyledContent>
+      <section></section>
+    </StyledContent>
+  );
+};
+
+const StyledContent = styled.div`
+  width: 100vw;
+  padding-top: 64px;
+
+  section {
+    padding: 8px;
+    min-height: 50vh;
+  }
+`;
+
+export default Content;
