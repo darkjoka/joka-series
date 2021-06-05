0afba233fe8bdfe80ed4a28fda43bbfd5f3e942c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-05 19:23:33 +0000

    feat: add close svg path to constants

--- client/src/constants/svg.ts
+++ client/src/constants/svg.ts
@@ -6,6 +6,9 @@ const bookMark =
 const bookMarkFilled =
   "M3 0h8a3 3 0 0 1 3 3v15a2 2 0 0 1-3.348 1.477L7.674 16.76a1 1 0 0 0-1.348 0l-2.978 2.717A2 2 0 0 1 0 18V3a3 3 0 0 1 3-3z" as const;
 
+const close =
+  "M7.314 5.9l3.535-3.536A1 1 0 1 0 9.435.95L5.899 4.485 2.364.95A1 1 0 1 0 .95 2.364l3.535 3.535L.95 9.435a1 1 0 1 0 1.414 1.414l3.535-3.535 3.536 3.535a1 1 0 1 0 1.414-1.414L7.314 5.899z" as const;
+
 const preserveAspectRatio = "xMinYMin" as const;
 
-export { viewBox, bookMark, bookMarkFilled, preserveAspectRatio };
+export { viewBox, bookMark, bookMarkFilled, preserveAspectRatio, close };
