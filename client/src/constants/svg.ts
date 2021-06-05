a5026943f164b1cac82006da71df785cf4a9c456
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-05 00:22:13 +0000

    feat: add svg constants

---
+++ client/src/constants/svg.ts
@@ -0,0 +1,11 @@
+const viewBox = "-5 -2 24 24" as const;
+
+const bookMark =
+  "M3 2a1 1 0 0 0-1 1v15l2.978-2.717a3 3 0 0 1 4.044 0L12 18V3a1 1 0 0 0-1-1H3zm0-2h8a3 3 0 0 1 3 3v15a2 2 0 0 1-3.348 1.477L7.674 16.76a1 1 0 0 0-1.348 0l-2.978 2.717A2 2 0 0 1 0 18V3a3 3 0 0 1 3-3z" as const;
+
+const bookMarkFilled =
+  "M3 0h8a3 3 0 0 1 3 3v15a2 2 0 0 1-3.348 1.477L7.674 16.76a1 1 0 0 0-1.348 0l-2.978 2.717A2 2 0 0 1 0 18V3a3 3 0 0 1 3-3z" as const;
+
+const preserveAspectRatio = "xMinYMin" as const;
+
+export { viewBox, bookMark, bookMarkFilled, preserveAspectRatio };
