95d715d050aa2b2f70b9310f567c6fd78ea61421
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-05 01:49:39 +0000

    fix: remove duplicate global styles from app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 import { GENERIC_BACKGROUND } from "./constants/colors";
-import GlobalStyle from "./components/GlobalStyle";
 
 const App: FC = () => {
   return (
     <>
-      <GlobalStyle></GlobalStyle>
       <Main></Main>
     </>
   );
