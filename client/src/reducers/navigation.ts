668072a674ab95cae8eceb3f2784a4700a6cb14d
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-06 23:09:01 +0000

    feat: complete nav reducer

--- client/src/reducers/navigation.ts
+++ client/src/reducers/navigation.ts
@@ -1,3 +1,10 @@
+import {
+  CLOSE_BOTTOM,
+  CLOSE_SIDE,
+  OPEN_BOTTOM,
+  OPEN_SIDE,
+} from "../constants/action";
+
 interface NavState {
   isSideNavOpen: boolean;
   isBottomSectOpen: boolean;
@@ -12,8 +19,26 @@ const defaultState: NavState = {
   isBottomSectOpen: false,
 };
 
-const navigation = (state: NavState = defaultState, action: Action) => {
-  return state;
+const navigation = (
+  state: NavState = defaultState,
+  action: Action
+): NavState => {
+  switch (action.type) {
+    case OPEN_SIDE:
+      return { ...state, isSideNavOpen: true };
+
+    case CLOSE_SIDE:
+      return { ...state, isSideNavOpen: false };
+
+    case OPEN_BOTTOM:
+      return { ...state, isSideNavOpen: false, isBottomSectOpen: true };
+
+    case CLOSE_BOTTOM:
+      return { ...state, isBottomSectOpen: false };
+
+    default:
+      return state;
+  }
 };
 
 export { navigation };
