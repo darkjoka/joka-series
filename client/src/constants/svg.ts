6bec08f5915fb5373fc83af6538fa9b9e7ac7904
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-06 23:23:54 +0000

    refactor: change svg constants to obj

--- client/src/constants/svg.ts
+++ client/src/constants/svg.ts
@@ -1,14 +1,26 @@
-const viewBox = "-5 -2 24 24" as const;
+interface Icon {
+  path: string;
+  viewBox: string;
+}
 
-const bookMark =
-  "M3 2a1 1 0 0 0-1 1v15l2.978-2.717a3 3 0 0 1 4.044 0L12 18V3a1 1 0 0 0-1-1H3zm0-2h8a3 3 0 0 1 3 3v15a2 2 0 0 1-3.348 1.477L7.674 16.76a1 1 0 0 0-1.348 0l-2.978 2.717A2 2 0 0 1 0 18V3a3 3 0 0 1 3-3z" as const;
+export const preserveAspectRatio = "xMinYMin";
 
-const bookMarkFilled =
-  "M3 0h8a3 3 0 0 1 3 3v15a2 2 0 0 1-3.348 1.477L7.674 16.76a1 1 0 0 0-1.348 0l-2.978 2.717A2 2 0 0 1 0 18V3a3 3 0 0 1 3-3z" as const;
+export const bookMark: Icon = {
+  path: "M3 2a1 1 0 0 0-1 1v15l2.978-2.717a3 3 0 0 1 4.044 0L12 18V3a1 1 0 0 0-1-1H3zm0-2h8a3 3 0 0 1 3 3v15a2 2 0 0 1-3.348 1.477L7.674 16.76a1 1 0 0 0-1.348 0l-2.978 2.717A2 2 0 0 1 0 18V3a3 3 0 0 1 3-3z",
+  viewBox: "-5 -2 24 24",
+} as const;
 
-const close =
-  "M7.314 5.9l3.535-3.536A1 1 0 1 0 9.435.95L5.899 4.485 2.364.95A1 1 0 1 0 .95 2.364l3.535 3.535L.95 9.435a1 1 0 1 0 1.414 1.414l3.535-3.535 3.536 3.535a1 1 0 1 0 1.414-1.414L7.314 5.899z" as const;
+export const bookMarkFilled: Icon = {
+  path: "M3 0h8a3 3 0 0 1 3 3v15a2 2 0 0 1-3.348 1.477L7.674 16.76a1 1 0 0 0-1.348 0l-2.978 2.717A2 2 0 0 1 0 18V3a3 3 0 0 1 3-3z",
+  viewBox: "-5 -2 24 24",
+} as const;
 
-const preserveAspectRatio = "xMinYMin" as const;
+export const close: Icon = {
+  path: "M7.314 5.9l3.535-3.536A1 1 0 1 0 9.435.95L5.899 4.485 2.364.95A1 1 0 1 0 .95 2.364l3.535 3.535L.95 9.435a1 1 0 1 0 1.414 1.414l3.535-3.535 3.536 3.535a1 1 0 1 0 1.414-1.414L7.314 5.899z",
+  viewBox: "-6 -6 24 24",
+} as const;
 
-export { viewBox, bookMark, bookMarkFilled, preserveAspectRatio, close };
+export const menu: Icon = {
+  path: "M1 0h5a1 1 0 1 1 0 2H1a1 1 0 1 1 0-2zm7 8h5a1 1 0 0 1 0 2H8a1 1 0 1 1 0-2zM1 4h12a1 1 0 0 1 0 2H1a1 1 0 1 1 0-2z",
+  viewBox: "-5 -7 24 24",
+} as const;
