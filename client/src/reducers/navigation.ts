943a9c08969d1df93216f4a63a2b49978d795147
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-06 22:46:15 +0000

    feat: initialize navigation reducer

---
+++ client/src/reducers/navigation.ts
@@ -0,0 +1,17 @@
+interface NavState {
+  isSideNavOpen: boolean;
+  isBottomSectOpen: boolean;
+}
+
+interface Action {
+  type: string;
+}
+
+const defaultState: NavState = {
+  isSideNavOpen: false,
+  isBottomSectOpen: false,
+};
+
+const navigation = (state: NavState = defaultState, action: Action) => {};
+
+export { navigation };
