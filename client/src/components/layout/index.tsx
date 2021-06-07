b779da0c811008912e1bed5952d8e3b907975a1e
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-07 07:11:54 +0000

    feat: add side nav to layout

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Content } from "./Content";
 import { TopNav } from "./TopNav";
+import { SideNav } from "./SideNav";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
+      <SideNav></SideNav>
       <TopNav></TopNav>
       <Content>{children}</Content>
     </>
