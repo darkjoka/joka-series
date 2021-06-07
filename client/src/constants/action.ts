cfda131b8dda3a7e24f3c4d4632e7ef365041509
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-07 21:51:09 +0000

    feat: add action consts for current state

--- client/src/constants/action.ts
+++ client/src/constants/action.ts
@@ -2,3 +2,5 @@ export const OPEN_SIDE = "OPEN_SIDE_NAV";
 export const CLOSE_SIDE = "CLOSE_SIDE_NAV";
 export const OPEN_BOTTOM = "OPEN_BOTTOM_NAV";
 export const CLOSE_BOTTOM = "CLOSE_BOTTOM_NAV";
+export const PUSH_LINK = "PUSH_LINK";
+export const PUSH_DATA = "PUSH_DATA";
