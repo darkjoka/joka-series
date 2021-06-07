25057cc65eb4d0e9f72f80cc54313c312beab89e
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-07 00:36:31 +0000

    feat: control nav with nav state

--- client/src/components/layout/SideNav.tsx
+++ client/src/components/layout/SideNav.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../reducers";
 import styled from "styled-components";
 import { preserveAspectRatio, close } from "../../constants/svg";
 
 const SideNav: React.FC = () => {
+  const sideNavigation = useSelector(
+    (state: RootState) => state.navigation.isSideNavOpen
+  );
   return (
-    <StyledNav>
+    <StyledNav sideNavStatus={sideNavigation}>
       <Icon>
         <path d={close.path}></path>
       </Icon>
@@ -12,13 +17,15 @@ const SideNav: React.FC = () => {
   );
 };
 
-const StyledNav = styled.nav`
+const StyledNav = styled.nav<{ sideNavStatus: boolean }>`
   position: fixed;
   height: 100vh;
   width: 70vw;
   overflow-y: scroll;
   background-color: white;
-  transform: translateX(-100vw);
+  transform: translateX(
+    ${({ sideNavStatus }) => (sideNavStatus ? "0vw" : "-100vw")}
+  );
   transition: transform 0.5s ease-in-out;
   z-index: 2;
   box-shadow: 8px 0px 14px 2px rgba(82, 82, 82, 0.6);
