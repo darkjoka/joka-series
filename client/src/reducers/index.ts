b0c18afcd8b19ffa92c142f132ef9f8e74721aee
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-07 22:28:17 +0000

    feat: add current to rootReducer

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { navigation } from "./navigation";
+import { current } from "./current";
 
-export const rootReducer = combineReducers({ navigation });
+export const rootReducer = combineReducers({ navigation, current });
 
 export type RootState = ReturnType<typeof rootReducer>;
