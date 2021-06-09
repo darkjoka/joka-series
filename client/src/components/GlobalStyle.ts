bb8d577688dcfec2a4c0aa3a5b7ff29922aea373
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-09 23:38:00 +0000

    feat: add fallback font for varela

--- client/src/components/GlobalStyle.ts
+++ client/src/components/GlobalStyle.ts
@@ -13,6 +13,7 @@ export default createGlobalStyle`
       margin: 0;
       padding: 0;
       box-sizing: border-box;
-      font-family: "varela"
+      font-family: "varela", "Arial Round",-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 `;
