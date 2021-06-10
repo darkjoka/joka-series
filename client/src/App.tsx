64eedb85fac210ec3629ce2853a0b9ce13552f14
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-10 19:26:47 +0000

    feat: remove app component dependency on state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,54 +1,21 @@
-import React, { FC } from "react";
+import React from "react";
 import styled from "styled-components";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "./reducers";
-import { closeSide } from "./actions/navigation";
 import { Layout } from "./components/layout";
 import { GENERIC_BACKGROUND } from "./constants/colors";
 
-const App: FC = () => {
-  const sideNavState = useSelector(
-    (state: RootState) => state.navigation.isSideNavOpen
-  );
-
-  const dispatch = useDispatch();
-
-  const handleClick = (): void => {
-    if (sideNavState) {
-      dispatch(closeSide());
-    }
-  };
-
+const App: React.FC = () => {
   return (
     <>
-      <Main sideNavOpen={sideNavState} onClick={handleClick}>
+      <Main>
         <Layout>PlaceHolder Value</Layout>
       </Main>
     </>
   );
 };
 
-const navOpenStyles = `
-  position: relative;
-  overflow: hidden;
-
-  &::after {
-    content: "";
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.3);
-    z-index: 1;
-    position: absolute;
-  }
-`;
-
-const Main = styled.div<{ sideNavOpen: boolean }>`
+const Main = styled.div`
   background-color: ${GENERIC_BACKGROUND};
   height: 100vh;
-
-  ${({ sideNavOpen }) => (sideNavOpen ? navOpenStyles : "")}
 `;
 
 export default App;
