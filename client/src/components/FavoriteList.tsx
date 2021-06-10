903043e45980085d6332736d7e308b91f3c16c14
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-10 17:16:07 +0000

    feat: initialize favorite, filter and history lists

---
+++ client/src/components/FavoriteList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FavoriteList = () => {
+  return <>This is the Favorite List</>;
+};
---
+++ client/src/components/FilterList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FilterList = () => {
+  return <>This is the Filtered List </>;
+};
