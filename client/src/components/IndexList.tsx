47809c0734607e6be732ceb425e0210ab04762f3
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-11 17:46:22 +0000

    feat: setup primitive index list

--- client/src/components/IndexList.tsx
+++ client/src/components/IndexList.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface Dstate {
+  title: String;
+}
+type state = Dstate[];
 
 export const IndexList = () => {
-  return <>This is the Index List</>;
+  const [state, setState] = useState<state>([{ title: "" }]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        setState(result.data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
+  return (
+    <>
+      {!loading &&
+        state?.map(({ title }, index) => {
+          return <p key={index}>{title}</p>;
+        })}
+      {loading && <p>Loading...</p>}
+      {error && <p>There was an Error... Please try again later</p>}
+    </>
+  );
 };
