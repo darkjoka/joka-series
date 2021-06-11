f35fbab5fa7a1ae0165421cf5a1212009baf771f
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-11 17:02:58 +0000

    feat: add theme constants

--- client/src/constants/action.ts
+++ client/src/constants/action.ts
@@ -4,3 +4,5 @@ export const OPEN_BOTTOM = "OPEN_BOTTOM_NAV";
 export const CLOSE_BOTTOM = "CLOSE_BOTTOM_NAV";
 export const PUSH_LINK = "PUSH_LINK";
 export const PUSH_DATA = "PUSH_DATA";
+export const TOGGLE_DARK = "TOGGLE_DARK";
+export const TOGGLE_LIGHT = "TOGGLE_LIGHT";
