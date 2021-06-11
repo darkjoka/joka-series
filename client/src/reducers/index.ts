3ff13c2be5d590e202f843c0ef500e29e18882d0
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-11 18:27:59 +0000

    feat: add indexP to main reducer

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -2,7 +2,13 @@ import { combineReducers } from "redux";
 import { navigation } from "./navigation";
 import { current } from "./current";
 import { theme } from "./theme";
+import { index } from "./indexP";
 
-export const rootReducer = combineReducers({ navigation, current, theme });
+export const rootReducer = combineReducers({
+  navigation,
+  current,
+  theme,
+  index,
+});
 
 export type RootState = ReturnType<typeof rootReducer>;
