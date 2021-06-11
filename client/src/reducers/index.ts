ccd98f827a6b207e89c6487806f026236e75f6c3
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-11 17:05:05 +0000

    feat: add theme to reducers

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import { navigation } from "./navigation";
 import { current } from "./current";
+import { theme } from "./theme";
 
-export const rootReducer = combineReducers({ navigation, current });
+export const rootReducer = combineReducers({ navigation, current, theme });
 
 export type RootState = ReturnType<typeof rootReducer>;
