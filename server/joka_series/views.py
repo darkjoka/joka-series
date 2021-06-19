e21b0e63858a1b3605c51fee2ecfd28bb5d4f894
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-19 17:43:13 +0000

    feat: add trailers and filter

--- server/joka_series/views.py
+++ server/joka_series/views.py
@@ -30,3 +30,20 @@ def detail(request, series):
     finalUrl = baseeUrl + series
     result: Dict[str, Union(str, list)] = scrapers.getDetails(finalUrl)
     return JsonResponse({"data": result}, json_dumps_params={"indent": 2})
+
+
+def trailers(request):
+    result = scrapers.getTrailers()
+    return JsonResponse({"data": result}, json_dumps_params={"indent": 2})
+
+
+def filter(request, type):
+    addUrl = (
+        f"tv-series-started-in-{type}"
+        if type.isnumeric()
+        else f"tv-series-{type}-genre"
+    )
+    finalUrl = baseUrl + addUrl
+    print(finalUrl)
+    result = scrapers.getFilteredSearch(finalUrl)
+    return JsonResponse({"data": result}, json_dumps_params={"indent": 2})
