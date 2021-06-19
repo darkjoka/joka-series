b2db59fc0df7555bcd705561823f9d8a0dc54bc1
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-19 14:16:22 +0000

    feat: increase no of skeleton

--- client/src/components/Load.tsx
+++ client/src/components/Load.tsx
@@ -4,7 +4,7 @@ import { SMovieCard } from "./skeleton/SMovieCard";
 const Load = () => {
   return (
     <>
-      {[1, 2, 3, 4, 5].map((num) => {
+      {[1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13].map((num) => {
         return <SMovieCard key={num} teaser={true} />;
       })}
     </>
