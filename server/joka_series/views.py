3cb8dd5a13ff8da746d75fcdb6b2b53101b7c7a2
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-19 17:42:42 +0000

    feat: search func accepst searchTerm

--- server/joka_series/views.py
+++ server/joka_series/views.py
@@ -14,20 +14,19 @@ baseeUrl = "http://www.todaytvseries2.com/tv-series/"
 
 def index(request):
     result: List[Dict[str, str]] = scrapers.getFromIndex(baseeUrl)
-    return JsonResponse({"data": result},json_dumps_params={"indent":2})
+    return JsonResponse({"data": result}, json_dumps_params={"indent": 2})
 
 
-def search(request):
-    searchTerm = request.POST.get("search")
+def search(request, searchTerm):
     addUrl = (
         f"search-series?searchword={quote_plus(searchTerm)}&searchphrase=all&limit=0"
     )
     finalUrl = baseUrl + addUrl
-    result: List[Dict[str, str]] = scrapers.getSearchResults(finalUrl)
-    return JsonResponse(json.dumps({"data": result}, indent=2))
+    result: List[Dict[str, str]] = scrapers.getSearchResult(finalUrl)
+    return JsonResponse({"data": result}, json_dumps_params={"indent": 2})
 
 
 def detail(request, series):
     finalUrl = baseeUrl + series
     result: Dict[str, Union(str, list)] = scrapers.getDetails(finalUrl)
-    return JsonResponse(json.dumps({"data": result}, indent=2))
+    return JsonResponse({"data": result}, json_dumps_params={"indent": 2})
