5ef31310447b894630f1ea3d873543ab7b679d8d
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-19 15:22:48 +0000

    feat: remove all scrollbars

--- client/src/components/GlobalStyle.ts
+++ client/src/components/GlobalStyle.ts
@@ -16,5 +16,12 @@ export default createGlobalStyle`
       box-sizing: border-box;
       font-family: "varela", "Arial Round",-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+
+    scrollbar-width: none;
+
+    &::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+    }
   }
 `;
