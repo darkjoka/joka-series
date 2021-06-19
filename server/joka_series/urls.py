dbad676cc4588ddffa18107b0ddef283d0cf63cd
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-19 17:40:41 +0000

    feat: add trailers to path

--- server/joka_series/urls.py
+++ server/joka_series/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("search/<str:searchTerm>", views.search, name="search"),
     path("detail/<str:series>/", views.detail, name="detail"),
+    path("trailers/", views.trailers, name="trailers"),
 ]
