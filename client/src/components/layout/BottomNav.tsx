7a6eadc5bdb7fc58fedcf869d758f95e31b62d31
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-21 21:23:48 +0000

    feat: load data from api

--- client/src/components/layout/BottomNav.tsx
+++ client/src/components/layout/BottomNav.tsx
@@ -6,15 +6,23 @@ import { close, preserveAspectRatio } from "../../constants/svg";
 import { RootState } from "../../reducers";
 import { closeBottom } from "../../actions/navigation";
 import { SDetail } from "../skeleton/SDetail";
+import { Error } from "../Error";
+import { pushData } from "../../actions/current";
+import { DetailState } from "../../reducers/current";
+import { Detail } from "../Detail";
 
 const BottomNav: React.FC = () => {
-  const [bottomNavigation, link]: [boolean, string] = useSelector(
-    (state: RootState) => {
-      return [state.navigation.isBottomSectOpen, state.current.link];
-    }
-  );
+  const [bottomNavigation, link, detail]: [boolean, string, DetailState] =
+    useSelector((state: RootState) => {
+      return [
+        state.navigation.isBottomSectOpen,
+        state.current.link,
+        state.current.detail,
+      ];
+    });
 
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -23,9 +31,32 @@ const BottomNav: React.FC = () => {
   };
 
   useEffect(() => {
+    const handleCurrent = (data: DetailState): void => {
+      dispatch(pushData(data));
+      setLoading(false);
+    };
+
     setLoading(true);
-    //to-do: request data from api
-  }, [link]);
+    setError(false);
+
+    fetch("http://localhost:4000/per")
+      .then((response) => {
+        return response.json();
+      })
+
+      .then((result) => {
+        handleCurrent(result.data);
+      })
+
+      .catch((e) => {
+        setLoading(false);
+        setError(true);
+      });
+    return () => {
+      setLoading(false);
+      setError(false);
+    };
+  }, [link, dispatch]);
 
   return (
     <StyledNav isBottomNavOpen={bottomNavigation}>
@@ -34,7 +65,11 @@ const BottomNav: React.FC = () => {
           <path d={close.path}></path>
         </Icon>
       </InnerNav>
-      <Inner>{loading && <SDetail />}</Inner>
+      <Inner>
+        {!loading && !error && <Detail {...detail} />}
+        {loading && <SDetail />}
+        {error && <Error />}
+      </Inner>
     </StyledNav>
   );
 };
