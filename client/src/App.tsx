742ce64d09f2c3dc3f8e8bda24ddafb77014c471
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-23 20:49:58 +0000

    fix: remove extraneos divs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import styled from "styled-components";
 import { Layout } from "./components/layout";
-import { GENERIC_BACKGROUND } from "./constants/colors";
 
 const App: React.FC = () => {
-  return (
-    <>
-      <Main>
-        <Layout></Layout>
-      </Main>
-    </>
-  );
+  return <Layout />;
 };
-
-const Main = styled.div`
-  background-color: ${GENERIC_BACKGROUND};
-  height: 100vh;
-`;
 
 export default App;
