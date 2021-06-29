4fa2774976ed88767ebae5220d48d09c1763e004
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-06-29 20:58:39 +0000

    feat: augment theme state

--- client/src/reducers/theme.ts
+++ client/src/reducers/theme.ts
@@ -1,8 +1,19 @@
 import { TOGGLE_LIGHT, TOGGLE_DARK } from "../constants/action";
+import {
+  DARK_ONE,
+  DISCORD_DARK,
+  DISCORD_TEXT_ON_DARK,
+  DISCORD_YELLOW,
+  GENERIC_BACKGROUND,
+  GENERIC_BORDER,
+} from "../constants/colors";
 
-interface ThemeState {
+export interface ThemeState {
   isLight: boolean;
-  isDark: boolean;
+  primaryColor: string;
+  secondaryColor: string;
+  tertiaryColor: string;
+  accentColor: string;
 }
 
 interface Action {
@@ -11,7 +22,10 @@ interface Action {
 
 const defaultState: ThemeState = {
   isLight: true,
-  isDark: false,
+  primaryColor: GENERIC_BACKGROUND,
+  secondaryColor: DARK_ONE,
+  tertiaryColor: GENERIC_BORDER,
+  accentColor: DISCORD_YELLOW,
 };
 
 const theme = (
@@ -20,10 +34,22 @@ const theme = (
 ): ThemeState => {
   switch (action.type) {
     case TOGGLE_DARK:
-      return { isLight: false, isDark: true };
+      return {
+        ...state,
+        isLight: false,
+        primaryColor: DISCORD_DARK,
+        secondaryColor: DISCORD_TEXT_ON_DARK,
+        tertiaryColor: DARK_ONE,
+      };
 
     case TOGGLE_LIGHT:
-      return { isLight: true, isDark: false };
+      return {
+        ...state,
+        isLight: true,
+        primaryColor: GENERIC_BACKGROUND,
+        secondaryColor: DARK_ONE,
+        tertiaryColor: GENERIC_BORDER,
+      };
 
     default:
       return state;
