0b8ece1fe278b662ea721f9f839ba4c4e5ed7706
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-07-06 23:57:37 +0000

    feat: add app urls

---
+++ server/jokaseries/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = "jokaseries"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("search/<str:searchTerm>", views.search, name="search"),
+    path("detail/<str:series>/", views.detail, name="detail"),
+    path("trailers/", views.trailers, name="trailers"),
+    path("filter/<str:type>", views.filter, name="filter"),
+]
