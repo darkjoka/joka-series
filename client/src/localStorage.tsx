d390a8acc14b3f6aca287318c10a9b1bb546c0d8
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-08-14 22:15:32 +0000

    refactor: init localToggle

--- client/src/localStorage.tsx
+++ client/src/localStorage.tsx
@@ -2,6 +2,7 @@ import { Movie } from "./reducers/indexP";
 
 export const localFetch = (store: string): Movie[] => {
   const item = localStorage.getItem(store);
+
   if (item) {
     return JSON.parse(item);
   }
@@ -10,4 +11,22 @@ export const localFetch = (store: string): Movie[] => {
 
 export const localSet = (store: string, payLoad: Movie[]): void => {
   localStorage.setItem(store, JSON.stringify(payLoad));
+};
+
+export const localToggle = (store: string, currMovie: Movie): Movie[] => {
+  let items: Movie[] = localFetch(store);
+
+  if (
+    items.some((movie) => {
+      return movie.title === currMovie.title;
+    })
+  ) {
+    items = items.filter((series) => {
+      return series.title !== currMovie.title;
+    });
+  } else {
+    items = items.concat([{ ...currMovie }]);
+  }
+
+  return items;
 };
