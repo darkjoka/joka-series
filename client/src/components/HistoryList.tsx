b8f65f24dc5bcdc98ae3a56fd396c460ad1a9a73
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-08-14 22:32:21 +0000

    refactor: use updated local storage functions

--- client/src/components/HistoryList.tsx
+++ client/src/components/HistoryList.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import MovieDisplay from "./MovieDisplay";
-import { handleLocalFetch } from "./MovieCard";
 import { useSelector } from "react-redux";
 import { RootState } from "../reducers";
+import { localFetch } from "../localStorage";
 
 export const HistoryList = () => {
-  const [movies] = useState(handleLocalFetch("history"));
+  const [movies] = useState(localFetch("history"));
   const theme = useSelector((state: RootState) => state.theme);
   return (
     <>
