22e11d6a12c0bfb9977a66ed86b497a82b13f4c9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-11 16:39:14 +0000

    refactor: import and use MovieItemSkeleton

--- client/src/components/Load/index.tsx
+++ client/src/components/Load/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { ThemeState } from "../../shared/types/types";
-import { SMovieCard } from "../skeleton/MovieCardSkeleton";
+import { MovieItemSkeleton } from "../skeleton/MovieItemSkeleton";
 
 const Load: React.FC<{ theme: ThemeState }> = ({ theme }) => {
   return (
     <>
       {[1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13].map((num) => {
-        return <SMovieCard theme={theme} key={num} teaser={true} />;
+        return <MovieItemSkeleton theme={theme} key={num} teaser={true} />;
       })}
     </>
   );
