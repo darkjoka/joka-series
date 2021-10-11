83b0bedafd5395ece23c757e289fca7d5a49989c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-11 16:20:48 +0000

    refactor: rename MovieDisplay to Movie

--- client/src/components/Movie/index.tsx
+++ client/src/components/Movie/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DisplayProps } from "../../shared/types/types";
 import { MovieCard } from "../MovieCard";
 
-const MovieDisplay: React.FC<DisplayProps> = ({ movies, theme }) => {
+const Movie: React.FC<DisplayProps> = ({ movies, theme }) => {
   return (
     <>
       {movies.map(({ title, permaLink, imageSource, teaser }) => {
@@ -21,4 +21,4 @@ const MovieDisplay: React.FC<DisplayProps> = ({ movies, theme }) => {
   );
 };
 
-export default MovieDisplay;
+export default Movie;
