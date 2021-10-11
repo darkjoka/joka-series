3df155b30d7214c56528b8213ab047440a69db5b
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-11 16:31:18 +0000

    refactor: import and use MovieItem

--- client/src/components/Movie/index.tsx
+++ client/src/components/Movie/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { DisplayProps } from "../../shared/types/types";
-import { MovieCard } from "../MovieCard";
+import { MovieItem } from "../MovieItem";
 
 const Movie: React.FC<DisplayProps> = ({ movies, theme }) => {
   return (
     <>
       {movies.map(({ title, permaLink, imageSource, teaser }) => {
         return (
-          <MovieCard
+          <MovieItem
             theme={theme}
             permaLink={permaLink}
             key={permaLink}
