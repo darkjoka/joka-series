eb75d20162e586953b346ae9073a16d68c647112
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-11 18:32:37 +0000

    refactor: import components from right dir

--- client/src/views/Home/index.tsx
+++ client/src/views/Home/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import Movie from "../Movie";
-import { Load } from "../Load";
-import { Error } from "../Error";
+import Movie from "../../components/Movie";
+import { Load } from "../../components/Load";
+import { Error } from "../../components/Error";
 import { RootState } from "../../store/reducers";
 import { populate_index } from "../../store/actions/populate";
 import { MovieType, ThemeState } from "../../shared/types/types";
