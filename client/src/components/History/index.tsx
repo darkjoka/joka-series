d3f7c7b4f3e8ff284915b1f01c4d12006695cc3f
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-11 16:15:54 +0000

    refactor: rename HistoryList to History

--- client/src/components/History/index.tsx
+++ client/src/components/History/index.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store/reducers";
 import { localFetch } from "../../shared/localStorage";
 
-export const HistoryList = () => {
+export const History = () => {
   const [movies] = useState(localFetch("history"));
   const theme = useSelector((state: RootState) => state.theme);
   return (
