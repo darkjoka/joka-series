4c47f7d8a2d7de2eb04a14d761ce82e343d539e2
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-11 16:52:51 +0000

    refactor: move styling to new file

---
+++ client/src/components/Error/ErrorStyle.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ErrorBox = styled.div`
+  width: 100%;
+  min-height: 50vh;
+  display: grid;
+  place-items: center;
+  color: red;
+
+  p {
+    padding: 24px;
+    border: 1px solid red;
+    position: relative;
+  }
+`;
--- client/src/components/Error/index.tsx
+++ client/src/components/Error/index.tsx
@@ -1,25 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 
-const Error = () => {
+import { ErrorBox } from "./ErrorStyle";
+
+export const Error = () => {
   return (
     <ErrorBox>
       <p>There was An error</p>
     </ErrorBox>
   );
 };
-
-const ErrorBox = styled.div`
-  width: 100%;
-  min-height: 50vh;
-  display: grid;
-  place-items: center;
-  color: red;
-
-  p {
-    padding: 24px;
-    border: 1px solid red;
-    position: relative;
-  }
-`;
-export { Error };
