1cddbacd053ed5618c41bf883840f34dc6c61602
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-13 16:56:35 +0000

    refactor: call actions directly without dispatch func

--- client/src/elements/SearchItem/index.tsx
+++ client/src/elements/SearchItem/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 
 import { Container } from "./SearchItemStyle";
 import { ThemeState } from "../../shared/types/types";
@@ -11,11 +10,10 @@ export const SearchItem: React.FC<{
   title: string;
   theme: ThemeState;
 }> = ({ permaLink, title }) => {
-  const dispatch = useDispatch();
-
   const handleDownload = () => {
-    dispatch(pushLink(permaLink));
-    dispatch(openBottom());
+    pushLink(permaLink);
+    openBottom();
   };
+
   return <Container onClick={handleDownload}>{title}</Container>;
 };
