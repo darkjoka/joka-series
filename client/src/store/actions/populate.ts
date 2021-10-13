1de58d871c94318bf0503b22757f73549cfb0dae
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-13 16:40:50 +0000

    refactor: dispatch actions instead of returning them

--- client/src/store/actions/populate.ts
+++ client/src/store/actions/populate.ts
@@ -1,10 +1,11 @@
+import { store } from "..";
 import { POPULATE_FILTER, POPULATE_INDEX } from "../../shared/constants/action";
 import { Movies } from "../../shared/types/types";
 
-export const populate_index = (payLoad: Movies) => {
-  return { type: POPULATE_INDEX, payLoad };
+export const populate_index = (payLoad: Movies): void => {
+  store.dispatch({ type: POPULATE_INDEX, payLoad });
 };
 
-export const populate_filter = (payLoad: Movies) => {
-  return { type: POPULATE_FILTER, payLoad };
+export const populate_filter = (payLoad: Movies): void => {
+  store.dispatch({ type: POPULATE_FILTER, payLoad });
 };
