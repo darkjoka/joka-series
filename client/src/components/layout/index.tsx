98bc843c3c151a0fc6d4ade11139091f45d939cf
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-13 16:48:43 +0000

    refactor: call actions directly without dispatch func

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { TopNav } from "./TopNav";
@@ -10,16 +9,15 @@ import { augmentFavorite, augmentHistory } from "../../store/actions/local";
 import { isLocalEmpty, localFetch, localSet } from "../../shared/localStorage";
 
 export const Layout: React.FC = () => {
-  const dispatch = useDispatch();
   useEffect(() => {
     if (!isLocalEmpty("favorite")) {
       localSet("favorite", []);
       localSet("history", []);
     } else {
-      dispatch(augmentFavorite(localFetch("favorite")));
-      dispatch(augmentHistory(localFetch("history")));
+      augmentFavorite(localFetch("favorite"));
+      augmentHistory(localFetch("history"));
     }
-  }, [dispatch]);
+  }, []);
 
   return (
     <>
