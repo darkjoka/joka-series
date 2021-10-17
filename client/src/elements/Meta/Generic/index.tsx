3e0f1ca9d49b72b78eefd4513551a9fde3f6c3b9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-17 21:47:20 +0000

    feat: add meta to generic

--- client/src/elements/Meta/Generic/index.tsx
+++ client/src/elements/Meta/Generic/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 import { MetaProps } from "..";
 
-export const GenericMeta: React.FC<MetaProps> = ({ title }) => {
+export const GenericMeta: React.FC<MetaProps> = ({ title, description }) => {
   return (
     <>
       <title>{title}</title>
+      <meta name="description" content={description} />
     </>
   );
 };
