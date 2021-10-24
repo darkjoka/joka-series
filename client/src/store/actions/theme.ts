ad0dd713846f38365f00d3b2af10c758c6fff819
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-24 22:43:00 +0000

    feat: call getBrand as payload

--- client/src/store/actions/theme.ts
+++ client/src/store/actions/theme.ts
@@ -1,14 +1,15 @@
 import { TOGGLE_DARK, TOGGLE_DIM, TOGGLE_LIGHT } from "../../shared/constants/action";
 import { store } from "..";
+import { getBrand } from "../reducers/theme";
 
 export const toggleDark = (): void => {
-  store.dispatch({ type: TOGGLE_DARK });
+  store.dispatch({ type: TOGGLE_DARK, payload: getBrand() });
 };
 
 export const toggleLight = (): void => {
-  store.dispatch({ type: TOGGLE_LIGHT });
+  store.dispatch({ type: TOGGLE_LIGHT, payload: getBrand() });
 };
 
 export const toggleDim = (): void => {
-  store.dispatch({ type: TOGGLE_DIM });
+  store.dispatch({ type: TOGGLE_DIM, payload: getBrand() });
 };
