0c1461e6651faa83797f72adf225113ce92f65c3
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-24 22:40:32 +0000

    feat: use useTheme hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+
 import { Layout } from "./components/layout";
+import { useTheme } from "./shared/hooks/useTheme";
 
 const App: React.FC = () => {
+  useTheme();
   return <Layout />;
 };
 
