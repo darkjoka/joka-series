dda95faffe6a2bdd6a4d520de95854667dd6e810
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-24 18:32:56 +0000

    refactor: change theme reducer

--- client/src/store/reducers/theme.ts
+++ client/src/store/reducers/theme.ts
@@ -1,60 +1,26 @@
-import { TOGGLE_LIGHT, TOGGLE_DARK } from "../../shared/constants/action";
-import {
-  DARK_ONE,
-  DISCORD_DARK,
-  DISCORD_RED,
-  DISCORD_TEXT_ON_DARK,
-  GENERIC_BACKGROUND,
-  GENERIC_BORDER,
-  WHITE,
-} from "../../shared/constants/colors";
-import { ThemeState, BaseAction } from "../../shared/types/types";
+import { TOGGLE_LIGHT, TOGGLE_DARK, TOGGLE_DIM } from "../../shared/constants/action";
+import { BaseAction, Brand, ThemeState } from "../../shared/types/types";
+import { themes } from "../../themes";
+import { store } from "..";
+import { defaultBrand } from "../../themes/base";
 
-const defaultState: ThemeState = {
-  isLight: true,
-  primBG: GENERIC_BACKGROUND,
-  primaryColor: WHITE,
-  primaryInverse: DARK_ONE,
-  secondaryColor: DARK_ONE,
-  secondaryInverse: GENERIC_BACKGROUND,
-  tertiaryColor: GENERIC_BORDER,
-  gradColor: GENERIC_BORDER,
-  accentColor: DISCORD_RED,
-  shadow: GENERIC_BORDER,
-  border: "transparent",
-};
+const [light, dark, dim] = themes;
+
+const genStore = store.getState();
+const brand: Brand = genStore.brand;
+
+const defaultState: ThemeState = light(defaultBrand);
 
 const theme = (state: ThemeState = defaultState, action: BaseAction): ThemeState => {
   switch (action.type) {
     case TOGGLE_DARK:
-      return {
-        ...state,
-        isLight: false,
-        primBG: DARK_ONE,
-        primaryColor: DARK_ONE,
-        primaryInverse: DISCORD_TEXT_ON_DARK,
-        secondaryColor: DISCORD_DARK,
-        secondaryInverse: GENERIC_BACKGROUND,
-        tertiaryColor: DISCORD_DARK,
-        gradColor: DISCORD_DARK,
-        shadow: "transparent",
-        border: state.accentColor,
-      };
+      return dark(brand);
 
     case TOGGLE_LIGHT:
-      return {
-        ...state,
-        isLight: true,
-        primBG: GENERIC_BACKGROUND,
-        primaryColor: WHITE,
-        primaryInverse: DARK_ONE,
-        secondaryColor: DARK_ONE,
-        secondaryInverse: GENERIC_BACKGROUND,
-        tertiaryColor: GENERIC_BORDER,
-        gradColor: GENERIC_BACKGROUND,
-        shadow: GENERIC_BORDER,
-        border: "transparent",
-      };
+      return light(brand);
+
+    case TOGGLE_DIM:
+      return dim(brand);
 
     default:
       return state;
