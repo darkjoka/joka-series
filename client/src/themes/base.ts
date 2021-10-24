ce2743f9a213f2b7ff1fe319fc2e551aafd9c870
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-24 19:27:35 +0000

    fix: hsl syntax

--- client/src/themes/base.ts
+++ client/src/themes/base.ts
@@ -6,14 +6,15 @@ export const defaultBrand: Brand = {
   lightness: 50,
 };
 
-export const color = (hue: number, saturation: number, lighness: number): string => {
-  return `hsl(${hue},${saturation}%,${lighness}%)`;
+export const color = (hue: number, saturation: number, lightness: number): string => {
+  return `hsl(${hue}deg ${saturation}% ${lightness}%)`;
 };
 
 export const theme = (identifier: (brand: Brand) => ThemeProperties, brand: Brand): ThemeState => {
   const selectTheme = identifier(brand);
   return {
     name: selectTheme.name,
+    brand: selectTheme.brand,
     primaryText: selectTheme.text1,
     secondaryText: selectTheme.text2,
     primaryBackground: selectTheme.surface1,
