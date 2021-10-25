61985b8315f3b057684da3b0f56c17e0c5a374d2
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-25 21:27:12 +0000

    feat: apply theme colors to pattern

--- client/src/components/Pattern/PatternStyle.ts
+++ client/src/components/Pattern/PatternStyle.ts
@@ -4,7 +4,8 @@ export const Wrapper = styled.div`
   margin: -600px 0 0 0;
 `;
 
-export const Row = styled.div`
+export const Row = styled.div(
+  ({ theme: { theme } }) => `
   height: 60px;
   white-space: nowrap;
 
@@ -12,7 +13,7 @@ export const Row = styled.div`
     width: 0;
     height: 0;
     border: 150px solid transparent;
-    border-bottom: 300px solid #cc2133;
+    border-bottom: 300px solid ${theme.brand};
     position: relative;
     display: inline-block;
     margin-left: -70px;
@@ -21,23 +22,24 @@ export const Row = styled.div`
   }
 
   &:nth-child(5n) > div {
-    border-bottom-color: #cc2133;
+    border-bottom-color: ${theme.primaryText};
   }
   &:nth-child(5n + 1) > div {
-    border-bottom-color: #d35a45;
+    border-bottom-color: ${theme.secondaryText};
   }
   &:nth-child(5n + 2) > div {
-    border-bottom-color: #f6d633;
+    border-bottom-color: ${theme.secondaryBackground};
   }
   &:nth-child(5n + 3) > div {
-    border-bottom-color: #b8d6a2;
+    border-bottom-color: ${theme.tertiaryBackground};
   }
   &:nth-child(5n + 4) > div {
-    border-bottom-color: #1e3a50;
+    border-bottom-color: ${theme.altBackground};
   }
   &:after {
     content: "";
     display: block;
     clear: both;
   }
-`;
+`
+);
