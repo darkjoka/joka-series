3e28b8421f89195ea03f08f669ebc368281f1352
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-25 20:40:46 +0000

    feat: add lighten and darken utils

--- client/src/lib/utils-theme.ts
+++ client/src/lib/utils-theme.ts
@@ -17,8 +17,40 @@ export const getThemeVariables = () => {
  * Add transparency to color value
  * @param  {string} color
  * @param  {number} transparencyValue
- * @return {string}
+ * @return string
  */
 export const opacify = (color: string, transparencyValue: number): string => {
   return color.substring(0, color.lastIndexOf(")")) + ` / ${transparencyValue}%)`;
+};
+
+/**
+ * Lightens color by factor[0-1]
+ * @param  {string} color
+ * @param  {number} factor
+ * @returns string
+ */
+export const lighten = (color: string, factor: number): string => {
+  const charBegin = color.lastIndexOf(" ");
+  const charEnd = color.lastIndexOf("%");
+  let lightnessValue = parseInt(color.substring(charBegin, charEnd));
+  lightnessValue += lightnessValue * (1 - factor);
+  if (lightnessValue > 100) lightnessValue = 100;
+  if (lightnessValue < 0) lightnessValue = 0;
+  return color.substring(0, charBegin) + ` ${lightnessValue}%)`;
+};
+
+/**
+ * Darkens color by factor[0-1]
+ * @param  {string} color
+ * @param  {number} factor
+ * @returns string
+ */
+export const darken = (color: string, factor: number): string => {
+  const charBegin = color.lastIndexOf(" ");
+  const charEnd = color.lastIndexOf("%");
+  let lightnessValue = parseInt(color.substring(charBegin, charEnd));
+  lightnessValue -= (lightnessValue * (factor * 2)) / 2;
+  if (lightnessValue > 100) lightnessValue = 100;
+  if (lightnessValue < 0) lightnessValue = 0;
+  return color.substring(0, charBegin) + ` ${lightnessValue}%)`;
 };
