f614cfa2d41db7023e850f6fc86bce69a7fcedb9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-25 17:51:01 +0000

    feat: add opacity utility func

--- client/src/lib/utils-theme.ts
+++ client/src/lib/utils-theme.ts
@@ -12,3 +12,13 @@ export const getThemeVariables = () => {
 
   return visual;
 };
+
+/**
+ * Add transparency to color value
+ * @param  {string} color
+ * @param  {number} transparencyValue
+ * @return {string}
+ */
+export const opacify = (color: string, transparencyValue: number): string => {
+  return color.substring(0, color.lastIndexOf(")")) + ` / ${transparencyValue}%)`;
+};
