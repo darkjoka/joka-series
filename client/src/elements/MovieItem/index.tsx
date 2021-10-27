dec88030e66ecd5ca7fde9ee4bf9a3f9bc87f663
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-27 15:25:08 +0000

    refactor: remove local func definitions to local-utils

--- client/src/elements/MovieItem/index.tsx
+++ client/src/elements/MovieItem/index.tsx
@@ -1,39 +1,19 @@
 import React, { useState } from "react";
 
-import { pushLink } from "../../store/actions/current";
+import { pushLink, pushThumbUrl } from "../../store/actions/current";
 import { openBottom } from "../../store/actions/navigation";
 import { augmentFavorite } from "../../store/actions/local";
 import { Movies, MovieProps } from "../../shared/types/types";
-import { localAdd, localFetch, localSet } from "../../shared/localStorage";
 import { bookMark, bookMarkFilled, camera } from "../../shared/constants/svg";
 import { Card, ImageHold, CamIcon, CardContent, Title, AltButtons, Download, Icon, Favorite } from "./MovieItemStyle";
+import { localAdd, localFetch, localSet } from "../../shared/lib/local-utils";
 
 export const MovieItem: React.FC<MovieProps> = ({ imageSource, title, teaser, permaLink }) => {
   const [localFavoriteStore, setLocalFavoriteStore] = useState<Movies>(localFetch("favorite"));
 
-  const updateLocal = (items: Movies, store: string): void => {
-    localSet(store, items);
-    setLocalFavoriteStore(items);
-  };
-
-  const syncLocal = (movies: Movies): void => {
-    // syncs app state with localStorage
-    augmentFavorite(movies);
-  };
-
-  const handleBookmark = (): void => {
-    let favorites = localAdd("favorite", {
-      title,
-      teaser,
-      permaLink,
-      imageSource,
-    });
-    updateLocal(favorites, "favorite");
-    syncLocal(favorites);
-  };
-
   const handleDownload = () => {
     pushLink(permaLink);
+    pushThumbUrl(imageSource);
     openBottom();
   };
 
@@ -51,7 +31,7 @@ export const MovieItem: React.FC<MovieProps> = ({ imageSource, title, teaser, pe
         <Title onClick={handleDownload}>{title}</Title>
         {teaser ? <p>{teaser.slice(0, 70)}...</p> : ""}
         <AltButtons>
-          <Favorite onClick={handleBookmark}>
+          <Favorite onClick={() => console.log("favorited")}>
             {localFavoriteStore.some((movie) => {
               return movie.title === title;
             })
@@ -62,7 +42,7 @@ export const MovieItem: React.FC<MovieProps> = ({ imageSource, title, teaser, pe
         </AltButtons>
       </CardContent>
 
-      <Icon onClick={handleBookmark}>
+      <Icon onClick={() => console.log("favorited")}>
         <path
           d={
             localFavoriteStore.some((movie) => {
