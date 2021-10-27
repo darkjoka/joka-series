7ec48a5978e2507792d3adfeb0aaf8b728d6a2d9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-27 15:20:44 +0000

    refactor: migrate localstorage funcs to utils-local

--- client/src/shared/lib/local-utils.ts
+++ client/src/shared/lib/local-utils.ts
@@ -1,4 +1,8 @@
-import { Movies, MovieType } from "./types/types";
+import Movie from "../../components/Movie";
+import { pushLink, pushThumbUrl } from "../../store/actions/current";
+import { augmentFavorite } from "../../store/actions/local";
+import { openBottom } from "../../store/actions/navigation";
+import { Movies, MovieType } from "../types/types";
 
 export const localFetch = (store: string): Movies => {
   // fetch and return movies from specified store
@@ -39,3 +43,28 @@ export const localAdd = (store: string, currMovie: MovieType, toggle: boolean = 
 
   return items;
 };
+
+export const updateLocal = (
+  items: Movies,
+  store: string,
+  callback: (data: Movies) => React.Dispatch<React.SetStateAction<Movies>>
+): void => {
+  localSet(store, items);
+  callback(items);
+};
+
+const syncLocal = (movies: Movies): void => {
+  // syncs app state with localStorage
+  augmentFavorite(movies);
+};
+
+// export const handleBookmark = ({ title, teaser, permaLink, imageSource }): void => {
+//   let favorites = localAdd("favorite", {
+//     title,
+//     teaser,
+//     permaLink,
+//     imageSource,
+//   });
+//   //   updateLocal(favorites, "favorite");
+//   syncLocal(favorites);
+// };
