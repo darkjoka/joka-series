78b28a0709bd16171ea79bb211231bc07c191b16
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-27 20:08:25 +0000

    feat: use local and setlocal from uselocal hook

--- client/src/components/Movie/index.tsx
+++ client/src/components/Movie/index.tsx
@@ -3,12 +3,20 @@ import React from "react";
 import { MovieItem } from "../../elements/MovieItem";
 import { DisplayProps } from "../../shared/types/types";
 
-const Movie: React.FC<DisplayProps> = ({ movies }) => {
+const Movie: React.FC<DisplayProps> = ({ movies, local, setLocal }) => {
   return (
     <>
       {movies.map(({ title, permaLink, imageSource, teaser }) => {
         return (
-          <MovieItem permaLink={permaLink} key={permaLink} title={title} imageSource={imageSource} teaser={teaser} />
+          <MovieItem
+            permaLink={permaLink}
+            key={permaLink}
+            title={title}
+            imageSource={imageSource}
+            teaser={teaser}
+            local={local}
+            setLocal={setLocal}
+          />
         );
       })}
     </>
