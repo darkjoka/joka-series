e70bdf6789ced138fc4e2162f2d225dadb378e0c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-28 17:11:04 +0000

    feat: initialize useFetch hook

---
+++ client/src/shared/hooks/useFetch/index.ts
@@ -0,0 +1,33 @@
+import React from "react";
+
+type Callback = ((...args: any[]) => void) | null;
+
+export const useFetch = (link: string, callback: Callback = null, condition: boolean = true) => {
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState();
+
+  React.useEffect(() => {
+    if (condition) {
+      setLoading(true);
+      setError(false);
+      (async () => {
+        try {
+          const response = await fetch("https://jokaseries.herokuapp.com/");
+          const result = await response.json();
+          if (callback) callback(result.data);
+          setData(result.data);
+        } catch (e) {
+          setLoading(false);
+          setError(true);
+        }
+      })();
+    }
+    return () => {
+      setLoading(false);
+      setError(false);
+    }; //set states to defaults on unmount => prevent prob of memory leak
+  }, [condition, callback]);
+
+  return [error, loading, data];
+};
