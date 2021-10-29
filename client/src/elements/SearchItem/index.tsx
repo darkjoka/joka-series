c0d3a526902e9179701ca5941551ea5174c667c2
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-29 00:46:24 +0000

    fix: push only essentials to store

--- client/src/elements/SearchItem/index.tsx
+++ client/src/elements/SearchItem/index.tsx
@@ -4,10 +4,10 @@ import { Container } from "./SearchItemStyle";
 import { pushLink } from "../../store/actions/current";
 import { openBottom } from "../../store/actions/navigation";
 
-export const SearchItem: React.FC<{
-  permaLink: string;
-  title: string;
-}> = ({ permaLink, title }) => {
+export const SearchItem: React.FC<{ permaLink: string; title: string }> = ({ permaLink, title }) => {
+  const essentials = permaLink.split("/");
+  permaLink = essentials[essentials.length - 1];
+
   const handleDownload = () => {
     pushLink(permaLink);
     openBottom();
