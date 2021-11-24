c20db508e5ae06a2535735794910312fb6780dce
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-11-24 11:59:13 +0000

    feat: add history components

--- client/src/pages/history.tsx
+++ client/src/pages/history.tsx
@@ -1,5 +1,15 @@
+import Movie from "../components/Movie";
+import { useLocal } from "../shared/hooks/useLocal";
+import { Movies } from "../shared/types/types";
+
 const History = () => {
-  return <p>History page</p>;
+  const [movies] = useLocal("history", [] as Movies);
+  const [local, setLocal] = useLocal("favorite", [] as Movies);
+  return (
+    <>
+      <Movie movies={movies} local={local} setLocal={setLocal} />
+    </>
+  );
 };
 
 export default History;
