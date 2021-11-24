730b96ec2d2cd541bacf447de7cb8d9609345b1b
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-11-24 11:58:04 +0000

    feat: add general navigation

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,9 +1,34 @@
 import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+
+import { getThemeVariables } from "../lib/utils-theme";
+import { store } from "../store";
+import GlobalStyle from "../shared/GlobalStyle";
+
+import { Hero } from "../components/layout/Hero";
+import { SideNav } from "../components/layout/SideNav";
+import { TopNav } from "../components/layout/TopNav";
+import { Content } from "../components/layout/Content";
+import { useTheme } from "../shared/hooks/useTheme";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return<>
-    <Component {...pageProps} />;
+  useTheme();
+  return (
+    <>
+      <Provider store={store}>
+        <GlobalStyle />
+        <ThemeProvider theme={getThemeVariables()}>
+          <SideNav />
+          <TopNav />
+          <Hero />
+          <Content>
+            <Component {...pageProps} />
+          </Content>
+        </ThemeProvider>
+      </Provider>
     </>
+  );
 };
 
 export default App;
