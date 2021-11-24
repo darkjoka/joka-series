bdac1eb88e27cde0d5f2381195889fbe73f43cc8
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-11-24 11:58:53 +0000

    feat: add favorite components

--- client/src/pages/favorite.tsx
+++ client/src/pages/favorite.tsx
@@ -1,5 +1,15 @@
+import { useLocal } from "../shared/hooks/useLocal";
+import { Movies } from "../shared/types/types";
+import MovieDisplay from "../components/Movie";
+
 const Favorites = () => {
-  return <p>Favorites page</p>;
+  const [favorites, setFavorites] = useLocal("favorite", [] as Movies);
+
+  return (
+    <>
+      <MovieDisplay movies={favorites} local={favorites} setLocal={setFavorites} />
+    </>
+  );
 };
 
 export default Favorites;
