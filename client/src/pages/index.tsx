eb62536b471feb6347ce8e7727bda3fd31a67050
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-11-24 11:57:30 +0000

    feat: add index component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,26 @@
-const Home = () => <>Hello from next</>;
+import { useSelector } from "react-redux";
 
-export default Home
+import { Load } from "../components/Load";
+import Movie from "../components/Movie";
+import { Error } from "../elements/Error";
+import { useFetch } from "../shared/hooks/useFetch";
+import { useLocal } from "../shared/hooks/useLocal";
+import { Movies } from "../shared/types/types";
+import { populate_index } from "../store/actions/populate";
+import { RootState } from "../store/reducers";
+
+const Home = () => {
+  const movies: Movies = useSelector((state: RootState) => state.index);
+  const [local, setLocal] = useLocal("favorite", [] as Movies);
+  const [error, loading] = useFetch("https://jokaseries.herokuapp.com/", populate_index, movies.length === 0);
+
+  return (
+    <>
+      {!loading && !error && <Movie movies={movies} local={local} setLocal={setLocal} />}
+      {loading && <Load />}
+      {error && <Error />}
+    </>
+  );
+};
+
+export default Home;
